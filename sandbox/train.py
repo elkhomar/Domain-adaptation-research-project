d391fdd0dcb7de2366633615bfe11990cf7ea95c
marionchadal <marion.chadal@gmail.com>
2024-02-03 15:33:22 +0100

    small comment added

--- sandbox/train.py
+++ sandbox/train.py
@@ -9,7 +9,6 @@ train_mnist, test_mnist = load_mnist(batch_size=64)
 model = FeatureExtractorCNN()
 criterion = nn.CrossEntropyLoss()
 
-# Assuming Trainer and Evaluator are defined in the same script or imported correctly
 trainer = Trainer(model=model, criterion=criterion)
 evaluator = Evaluator(model=model)
 
@@ -17,8 +16,6 @@ num_epochs = 50
 weight_wasserstein = 0.25
 loss_function_str = 'wasserstein'
 
-# Use the trainer instance to train the model
 trainer.train(source_loader=train_mnist, target_loader=train_usps, num_epochs=num_epochs, weight_discrepancy=weight_wasserstein, loss_function_str=loss_function_str)
 
-# Use the evaluator instance to evaluate the model
 evaluator.evaluate(test_loader=test_usps, device='cpu')
