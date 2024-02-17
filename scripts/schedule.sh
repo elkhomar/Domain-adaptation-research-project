e6d7cea1e249aeab17b07f2d45d67c6b8ad97f28
elkhomar <omar.elkhalifi16180@gmail.com>
2024-02-17 05:40:43 +0100

    corrected schedule.sh

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -3,5 +3,5 @@
 # Run from root folder with: bash scripts/schedule.sh
 
 #python src/train.py -m  experiment=e_ada model.lambd=0.5,1.0,1.5,2.0 data.batch_size=16,32,64
-python train.py -m hparams_search=lambda_search experiment=e_ada data.flip_domain=True logger.wandb.group="unbiased Wass usps to mnist"
-python train.py -m hparams_search=lambda_search experiment=e_ada data.flip_domain=False logger.wandb.group="unbiased Wass mnist to usps"
+python src/train.py -m hparams_search=lambda_search experiment=e_ada data.flip_domain=True logger.wandb.group="unbiased Wass usps to mnist"
+python src/train.py -m hparams_search=lambda_search experiment=e_ada data.flip_domain=False logger.wandb.group="unbiased Wass mnist to usps"
