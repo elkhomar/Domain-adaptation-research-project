401e6c8393a4a5b434500bff0680fd3e238855d2
sebvol <sebastien.vol@telecom-sudparis.eu>
2024-03-13 15:52:08 +0100

    mlp not finished

--- runs.sh
+++ runs.sh
@@ -1,2 +1,2 @@
-python src/train.py -m experiment=e_seb trainer.accelerator="gpu" model.lambd=0.01,0.15,0.2,0.25,0.3,0.5,0.8,1 data.batch_size=256 model.loss.kernel.n_kernels=2,4,5,6,7,8
-python src/train.py -m experiment=e_seb trainer.accelerator="gpu" model.lambd=0.01,0.15,0.2,0.25,0.3,0.5,0.8,1 data.batch_size=256 model.loss.kernel.n_kernels=2,4,5,6,7,8 data.flip_domain=true
+python src/train.py -m experiment=e_seb trainer.accelerator="gpu" model.lambd=0.7 data.batch_size=512 data.flip_domain=false seed=42,55,35,18,19 model.loss.kernel.n_kernels=8
+python src/train.py -m experiment=e_seb trainer.accelerator="gpu" model.lambd=0.1 data.batch_size=256 data.flip_domain=true seed=42,55,35,18,19 model.loss.kernel.n_kernels=3
